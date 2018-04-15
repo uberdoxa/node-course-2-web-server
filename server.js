70075dcde07583187cbf6a397e7c3d60df3149eb
ayo wynter <ayo.wynter@yahoo.com>
2018-04-14 22:45:12 -0400

    Add Project page

--- server.js
+++ server.js
@@ -54,6 +54,17 @@ app.get('/about',(req,res)=>{
 
 });
 
+//
+app.get('/projects',(req,res)=>{
+
+  //res.send('About Page');
+   res.render('projects.hbs',{
+        pageTitle: 'Projects',
+
+   });
+
+});
+
 app.get('/bad',(req,res)=>{
 
   res.send({errorMessage: 'Unable to handle request'});
